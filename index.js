8bd20d20acf0c09d77c043658b0b90d8bba969b1
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-11-01 10:39:22 -0500

    commit tree 6f35ed1424155d6dc04332ee631e33929b251333

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = '*') {
+    return function (word = 'special') {
+        return `You are ${flair}${word}${flair}!`
+    }
+}
+
+//function outer(greeting, msg = "It's a fine day to learn") {
+    //return function (name, lang = "Python") {
+      //return `${greeting}, ${name}! ${msg} ${lang}`;
+    //};
+  //}
